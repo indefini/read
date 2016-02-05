a2e9b2876df901b897e7eebc5386f2c0655da5db
Christophe Sadoine <chris@indefini.org>
2016-02-05 18:47:26 +0900

    test

--- elementary-sys/src/elementary.c
+++ elementary-sys/src/elementary.c
@@ -37,11 +37,26 @@ Evas_Object* window_new()
   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
   elm_win_resize_object_add(win, bg);
 
-  evas_object_resize(win, 300,500);
+  //evas_object_resize(win, 300,500);
+  evas_object_resize(win, 60,100);
   evas_object_show(win);
   return win;
 }
 
+struct _Images
+{
+  Eina_List* before_str;
+  Eina_List* before_img;
+  Eo* current;
+  Eina_List* after;
+  Eina_List* after_str;
+
+  Eo* win;
+};
+typedef struct _Images Images;
+
+
+Eo* _images;
 Eo* img;
 rust_cb _previous;
 rust_cb _next;
@@ -72,11 +87,11 @@ n_finger_tap_end(void *data , void *event_info)
    printf("x = %d, w = <%d>\n", p->x, w);
    if (p->x > w/2) {
      _next(_data);
-     elm_slideshow_next(_slide);
+     //elm_slideshow_next(_slide);
    }
    else {
      _previous(_data);
-     elm_slideshow_previous(_slide);
+     //elm_slideshow_previous(_slide);
    }
 
    return EVAS_EVENT_FLAG_ON_HOLD;
@@ -89,6 +104,95 @@ n_finger_tap_abort(void *data , void *event_info)
    printf("N tap abort\n");
    return EVAS_EVENT_FLAG_ON_HOLD;
 }
+
+static Evas_Event_Flags
+zoom_start(void *data , void *event_info)
+{
+   Elm_Gesture_Zoom_Info *p = (Elm_Gesture_Zoom_Info *) event_info;
+   //_color_and_icon_set(data, ZOOM_NAME, MAX_ZOOM, MAX_ZOOM, START_COLOR);
+   printf("zoom started <%d,%d> zoom=<%f> radius=<%d> momentum=<%f>\n",
+         p->x, p->y, p->zoom, p->radius, p->momentum);
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+static Evas_Event_Flags
+zoom_move(void *data , void *event_info)
+{
+   Elm_Gesture_Zoom_Info *p = (Elm_Gesture_Zoom_Info *) event_info;
+   //_color_and_icon_set(data, ZOOM_NAME, MAX_ZOOM, MAX_ZOOM, MOVE_COLOR);
+   printf("zoom move <%d,%d> zoom=<%f> radius=<%d> momentum=<%f>\n",
+         p->x, p->y, p->zoom, p->radius, p->momentum);
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+static Evas_Event_Flags
+zoom_end(void *data , void *event_info)
+{
+   Elm_Gesture_Zoom_Info *p = (Elm_Gesture_Zoom_Info *) event_info;
+   //_color_and_icon_set(data, ZOOM_NAME, MAX_ZOOM, MAX_ZOOM, END_COLOR);
+   printf("zoom end <%d,%d> zoom=<%f> radius=<%d> momentum=<%f>\n",
+         p->x, p->y, p->zoom, p->radius, p->momentum);
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+static Evas_Event_Flags
+zoom_abort(void *data , void *event_info EINA_UNUSED)
+{
+   printf("zoom abort\n");
+   //_color_and_icon_set(data, ZOOM_NAME, MAX_ZOOM, MAX_ZOOM, ABORT_COLOR);
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+static Evas_Event_Flags
+flick_start(void *data , void *event_info)
+{
+   Elm_Gesture_Line_Info *p = (Elm_Gesture_Line_Info *) event_info;
+   //_color_and_icon_set(data, FLICK_NAME, p->momentum.n, MAX_FLICK, START_COLOR);
+
+   printf("flick started x1,y1=<%d,%d> tx,ty=<%u,%u> n=<%u>\n",
+         p->momentum.x1, p->momentum.y1, p->momentum.tx,
+         p->momentum.ty, p->momentum.n);
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+static Evas_Event_Flags
+flick_end(void *data , void *event_info)
+{
+   Elm_Gesture_Line_Info *p = (Elm_Gesture_Line_Info *) event_info;
+   //_color_and_icon_set(data, FLICK_NAME, p->momentum.n, MAX_FLICK, END_COLOR);
+
+   printf("flick ended angle=%f, x1,y1=<%d,%d> x2,y2=<%d,%d> tx,ty=<%u,%u> mx=<%d> my=<%d> n=<%u>\n",
+         p->angle,
+         p->momentum.x1, p->momentum.y1,
+         p->momentum.x2, p->momentum.y2,
+         p->momentum.tx, p->momentum.ty,
+         p->momentum.mx, p->momentum.my,
+         p->momentum.n);
+
+   if (p->momentum.mx < 0) {
+     _next(_data);
+     elm_slideshow_next(_slide);
+   }
+   else {
+     _previous(_data);
+     elm_slideshow_previous(_slide);
+   }
+
+
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+static Evas_Event_Flags
+flick_abort(void *data , void *event_info)
+{
+   Elm_Gesture_Line_Info *p = (Elm_Gesture_Line_Info *) event_info;
+   //_color_and_icon_set(data, FLICK_NAME, p->momentum.n, MAX_FLICK, ABORT_COLOR);
+   printf("flick abort\n");
+   return EVAS_EVENT_FLAG_ON_HOLD;
+}
+
+
+
 
 void _create_rect(Evas_Object* win)
 {
@@ -106,12 +210,33 @@ void _create_rect(Evas_Object* win)
 
   Eo* infra = NULL;
 
+  //TAP
   elm_gesture_layer_cb_set(g, ELM_GESTURE_N_TAPS,
         ELM_GESTURE_STATE_START, n_finger_tap_start, infra);
   elm_gesture_layer_cb_set(g, ELM_GESTURE_N_TAPS,
         ELM_GESTURE_STATE_END, n_finger_tap_end, infra);
   elm_gesture_layer_cb_set(g, ELM_GESTURE_N_TAPS,
         ELM_GESTURE_STATE_ABORT, n_finger_tap_abort, infra);
+
+  //ZOOM
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_ZOOM,
+        ELM_GESTURE_STATE_START, zoom_start, infra);
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_ZOOM,
+        ELM_GESTURE_STATE_END, zoom_end, infra);
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_ZOOM,
+        ELM_GESTURE_STATE_ABORT, zoom_abort, infra);
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_ZOOM,
+        ELM_GESTURE_STATE_MOVE, zoom_move, infra);
+
+  //FLICK
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_N_FLICKS,
+        ELM_GESTURE_STATE_START, flick_start, infra);
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_N_FLICKS,
+        ELM_GESTURE_STATE_END, flick_end, infra);
+  elm_gesture_layer_cb_set(g, ELM_GESTURE_N_FLICKS,
+        ELM_GESTURE_STATE_ABORT, flick_abort, infra);
+
+
 
 
   /*
@@ -135,12 +260,16 @@ void ui_create(Evas_Object* win, void* data, rust_cb previous, rust_cb next)
   _data = data;
   _win = win;
 
+  _images = calloc(1, sizeof *_images);
+  _images->win = win;
+
   Eo* box = elm_box_add(win);
   evas_object_show(box);
   evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
   elm_win_resize_object_add(win, box);
 
   img = elm_image_add(win);
+  _images->current;
   //const char* path = "/home/chris/tmp/08/001.jpg";
   //elm_image_file_set(img, path, NULL);
   evas_object_size_hint_weight_set(img, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
@@ -148,6 +277,8 @@ void ui_create(Evas_Object* win, void* data, rust_cb previous, rust_cb next)
 
   elm_box_pack_end(box, img);
   evas_object_show(img);
+
+  _create_rect(win);
 }
 
 static Elm_Slideshow_Item_Class itc;
@@ -198,8 +329,84 @@ Eo* _slideshow_create(Evas_Object* win, void* data, rust_cb previous, rust_cb ne
 
 void image_add(Eo* slideshow, const char* path)
 {
-  Elm_Object_Item* item = 
-   elm_slideshow_item_add(slideshow, &itc, path);
+  if (slideshow) {
+    Elm_Object_Item* item = 
+     elm_slideshow_item_add(slideshow, &itc, strdup(path));
+  }
+
+
+  if (!_images) return;
+
+}
+
+Eo* _create_image(Eo* win, const char* path)
+{
+  Eo* im = elm_image_add(win);
+  elm_image_file_set(_images->current, path, NULL);
+  evas_object_size_hint_weight_set(img, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+  evas_object_size_hint_align_set(img, EVAS_HINT_FILL, EVAS_HINT_FILL);
+  
+  return im;
+}
+
+void image_add2(Images* images, const char* path)
+{
+  usize count = eina_list_count(images->after);
+
+  if (count > 2) {
+    images->after_str = 
+     eina_list_append(images->after_str, strdup(path));
+
+    return;
+  }
+
+  Eo* im = _create_image(images->win, path);
+
+  if (!_images->current) {
+    _images->current = im;
+    evas_object_show(im);
+  }
+  else {
+    images->after = eina_list_append(images->after, im);
+  }
+}
+
+void image_next2(Images* images)
+{
+  usize count = eina_list_count(images->after);
+
+  if (count == 0) return;
+
+  usize countbefore = eina_list_count(images->before);
+
+  if (countbefore > 2) {
+    const char* dance;
+    const char* group;
+    Eo* im = eina_list_data_get(images->before);
+    elm_image_file_get(im, &dance, &group);
+    images->before_str = 
+     eina_list_append(images->before_str, strdup(dance));
+
+    images->before = eina_list_next(images->before);
+
+    evas_object_del(im);
+  }
+
+  images->before = eina_list_append(images->before, images->current);
+
+  usize countafterstr = eina_list_count(images->after_str);
+
+  images->current = eina_list_data_get(images->after);
+
+  images->after = eina_list_next(images->after);
+
+  if (countafterstr > 0) {
+    const char* path = eina_list_data_get(images->after_str);
+    Eo* im = _create_image(images->win, path);
+    images->after = eina_list_append(images->after, im);
+    images->after_str = 
+     eina_list_next(images->after_str);
+  }
 }
 
 void image_clear(Eo* slideshow)
@@ -211,6 +418,7 @@ void image_clear(Eo* slideshow)
 
 void show_image(const char* path)
 {
-  elm_image_file_set(img, path, NULL);
+  printf("show image : %s \n ", path);
+  elm_image_file_set(img, strdup(path), NULL);
 }
 
